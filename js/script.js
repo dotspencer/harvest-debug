413f14ec02b179a8678a2b17c678a80fea5db06e
Spencer Smith <dontaskspencer@gmail.com>
2021-11-30 22:56:53 -0700

    Showing points with correct max/min and 500 mTorr line

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 const input = document.querySelector('input');
 const ctx = document.querySelector('canvas').getContext('2d');
+let chart;
 console.log('ctx:', ctx);
 
 input.addEventListener('change', handleFileInput);
 
 function handleFileInput(event) {
+  if (chart) {
+    chart.destroy();
+  }
   const file = input.files[0];
   const reader = new FileReader();
   reader.addEventListener('load', showGraph);
@@ -14,29 +18,40 @@ function handleFileInput(event) {
 function showGraph(event) {
   const { result } = event.target;
   const rows = parseData(result);
-  console.log('rows:', rows);
-  console.log('dataset data:', rows.map(r => parseInt(r['Pressure'])));
+  // console.log('rows:', rows);
+  // console.log('dataset data:', rows.map(r => parseInt(r['Pressure'])));
 
   const config = {
     type: 'line',
     data: {
-      labels: rows.map((r, i) => i),
+      labels: rows.map((r, i) => `T ${i}`),
       datasets: [{
-        label: 'My First Dataset',
+        label: 'Pressure (mTorr)',
         data: rows.map(r => Math.min(2500, parseInt(r['Pressure']))),
         fill: false,
         borderColor: 'rgb(75, 192, 192)',
         tension: 0.1
+      }, {
+        data: rows.map(r => 500),
+        pointRadius: 0,
       }],
     },
+    options: {
+      scales: {
+        y: {
+          min: 100,
+          max: 2500,
+        }
+      }
+    },
   };
-  const chart = new Chart(ctx, config);
+  chart = new Chart(ctx, config);
 }
 
 function parseData(raw) {
   const rows = raw.trim().split('\n');
   const labels = rows.shift().split(',');
-  console.log('labels:', labels);
+  // console.log('labels:', labels);
 
   const data = [];
 
